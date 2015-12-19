15cc944aecd3c4bcbcdabf3522eb407f54b421d3
Wojciech Gomoła <szogun1987@gmail.com>
2015-12-19 14:43:42 +0100

    Final commit

---
+++ src/MvvmLigthBugExposition/MvvmLigthBugExposition/ViewModels/DetailViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MvvmLigthBugExposition.ViewModels
+{
+    public class DetailViewModel
+    {
+        private readonly bool field;
+        public DetailViewModel()
+        {
+            bool variable = SomeComputation();
+            UseVariableCommand = new RelayCommand(Some, () => variable);
+
+            field = SomeComputation();
+            UseFieldCommand = new RelayCommand(Some, () => field);
+        }
+        
+        public ICommand UseVariableCommand { get; private set; }
+
+        public ICommand UseFieldCommand { get; private set; }
+
+        private void Some()
+        {
+            
+        }
+
+        private bool SomeComputation()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/MvvmLigthBugExposition/MvvmLigthBugExposition/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MvvmLigthBugExposition.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private DetailViewModel content;
+
+        public MainViewModel()
+        {
+            SetContent100PercentBugProbabilityCommand = new RelayCommand(SetContent100PercentBugProbability);
+            SetContent50PercentBugProbabilityCommand = new RelayCommand(SetContent50PercentBugProbability);
+        }
+
+        public DetailViewModel Content
+        {
+            get { return content; }
+            private set { Set(ref content, value); }
+        }
+
+        public ICommand SetContent100PercentBugProbabilityCommand { get; private set; }
+
+        public ICommand SetContent50PercentBugProbabilityCommand { get; private set; }
+
+        private void SetContent100PercentBugProbability()
+        {
+            Content = new DetailViewModel();
+            GC.Collect();
+        }
+
+        private void SetContent50PercentBugProbability()
+        {
+            Content = new DetailViewModel();
+            byte[] array = new byte[1024*1024*10];
+        }
+    }
+}
